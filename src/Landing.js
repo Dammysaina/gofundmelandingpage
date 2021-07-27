2665cb8a4633d2caa948bbbb37d28c7bdeab390f
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-07-27 15:49:46 +0100

    landing

---
+++ src/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import './Landing.css';
+
+const Landing = () => {
+    return (
+       <div>
+           <p>baby David</p>
+       </div>
+    )
+}
+
+export default Landing;
