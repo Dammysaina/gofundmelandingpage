4b526f8fe6b57857741bb631009521b7bc91ff02
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-08-02 16:32:19 +0100

    fundraising

---
+++ src/components/Fundraising/Fundraising.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"; 
+import "./Fundraising.css";
+
+export default class Fundraising extends Component {
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
