f3036253d8bd75f9fbf292a0a5b427672d85f443
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-08-06 14:15:28 +0100

    fundraising

--- src/components/Fundraising/Fundraising.js
+++ src/components/Fundraising/Fundraising.js
@@ -4,8 +4,18 @@ import "./Fundraising.css";
 export default class Fundraising extends Component {
     render () {
         return (
-            <div>
-                
+            <div className="fundRaise">
+                <div className="fundraiseOne">
+                    <div>
+                        <p>Fundraising for the people and causes you care about</p>
+                    </div>
+                    <div >
+                        <img src={`/images/Rectangle.svg`} alt="women smiling" />
+                    </div>
+                </div>
+                <div>
+
+                </div>
             </div>
         )
     }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,12 @@
 import React from "react"
+import Fundraising from "./Fundraising/Fundraising";
 import Layout from "./Layout";
 
 const Landing = () => {
     return (
        <div>
          <Layout>
-            
+         <Fundraising />
          </Layout> 
           
        </div>
