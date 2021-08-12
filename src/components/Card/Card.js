afcce968661447b0d245119c6f3d8c6e6497093a
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-08-12 15:14:36 +0100

    card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,36 @@
-import React , {Component} from "react"
+import React , {Component} from "react";
+import "./Card.css"
 
 export default class Card extends Component {
     render () {
         return (
-            <div className="card">
-                <img src={`/images/lamb.jpg`} alt=""/>
-                    <div className="card-text">
-                        <h3>Online Experiences</h3>
-                        <h4>Unique activities we can do together, led by world of hosts.</h4>
-                    </div>
+            <div className="pictureCard">
+                <div className="card">
+                    <img src={`/images/Rectangle (2).svg`} alt=""/>
+                        <div className="card-text">
+                            <h3>Massapequa, NY</h3>
+                            <p>Clive Family</p>
+                            <p>Tragically Barbara and Dave Clive passed away in the past week with…</p>
+                        </div>
+                </div>
+                <div className="card">
+                    <img src={`/images/Rectangle (3).svg`} alt=""/>
+                        <div className="card-text">
+                            <h3>Massapequa, NY</h3>
+                            <p>Clive Family</p>
+                            <p>Tragically Barbara and Dave Clive passed away in the past week with…</p>
+                        </div>
+                </div>
+                <div className="card">
+                    <img src={`/images/Rectangle (4).svg`} alt=""/>
+                        <div className="card-text">
+                            <h3>Massapequa, NY</h3>
+                            <p>Clive Family</p>
+                            <p>Tragically Barbara and Dave Clive passed away in the past week with…</p>
+                        </div>
+                </div>
             </div>
+            
         )
         
     }
--- src/components/Fundraiser/Fundraiser.js
+++ src/components/Fundraiser/Fundraiser.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react"; 
+import Card from "../Card/Card";
 import "./Fundraiser.css";
 
 export default class Fundraiser extends Component {
@@ -9,7 +10,7 @@ export default class Fundraiser extends Component {
                     <p>Top Fundraisers</p>
                 </div>
                 <div>
-
+                    <Card />
                 </div>
             </div>
         )
