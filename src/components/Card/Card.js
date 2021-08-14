dcef33f0e5e1455cadce36619c1c8fbd12aa02bb
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-08-14 19:25:12 +0100

    fundraiser done

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -22,18 +22,26 @@ export default class Card extends Component {
                     <img src={`/images/Rectangle (3).svg`} alt=""/>
                         <div className="card-text">
                             <h3>PROVIDENCE, RI</h3>
-                            <p>Clive Family</p>
-                            <p>Tragically Barbara and Dave Clive passed away in the past week with…</p>
-                            <p>Last donation 2m ago</p>
+                            <p className="cliveFamily">Help pay for AJs hospital treatment and recovery</p>
+                            <p className="cliveTragic">Tragically Barbara and Dave Clive passed away in the past week with…</p>
+                            <p className="lastDonate">Last donation 2m ago</p>
+                            <div className="progressBar">
+                                <p className="progressBarSpan"><span></span></p>
+                            </div>
+                            <p className="dollarRaised">$769,046 raised of $800,000</p>
                         </div>
                 </div>
                 <div className="card">
                     <img src={`/images/Rectangle (4).svg`} alt=""/>
                         <div className="card-text">
                             <h3>BALTIMORE, MD</h3>
-                            <p>Clive Family</p>
-                            <p>Tragically Barbara and Dave Clive passed away in the past week with…</p>
-                            <p>Last donation 2m ago</p>
+                            <p className="cliveFamily">Clive Family</p>
+                            <p className="cliveTragic">Tragically Barbara and Dave Clive passed away in the past week with…</p>
+                            <p className="lastDonate">Last donation 2m ago</p>
+                            <div className="progressBar">
+                                <p className="progressBarSpan"><span></span></p>
+                            </div>
+                            <p className="dollarRaised">$769,046 raised of $800,000</p>
                         </div>
                 </div>
             </div>
