882d2bd9202ec677a314311d6b6cdb57b8660b6b
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-08-16 15:49:59 +0100

    trending

---
+++ src/components/Trending/Trending.js
@@ -0,0 +1,11 @@
+import React , {Component} from "react"
+
+export default class trending extends Component {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
