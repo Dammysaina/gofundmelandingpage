be99775a3b4230efac3dc5c7386b093c5fb331f5
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-08-23 18:35:48 +0100

    People

---
+++ src/components/People/People.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import Peoplecard from "./Peoplecard";
+
+
+export default class People extends Component {
+    render () {
+        return (
+            <div>
+                <Peoplecard/>
+            </div>
+        )
+    }
+}
