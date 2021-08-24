4fd11e99b9e071d78b6b7646f2db64ed7cca2931
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-08-24 14:01:33 +0100

    people done

--- src/components/People/Peoplecard.js
+++ src/components/People/Peoplecard.js
@@ -22,7 +22,7 @@ export default class Peoplecard extends Component {
                             Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
                             Est eu quis anim excepteur et. ipsum
                         </h4>
-                        <p className="peopleType">Medical</p>
+                        <p className="peopleType">Memorial</p>
                     </div>
                     <div className="peopleCard">
                         <h4 className="peopleText">
@@ -31,7 +31,7 @@ export default class Peoplecard extends Component {
                             Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
                             Est eu quis anim excepteur et. ipsum
                         </h4>
-                        <p className="peopleType">Medical</p>
+                        <p className="peopleType">Emergency</p>
                     </div>
                     <div className="peopleCard">
                         <h4 className="peopleText">
@@ -40,7 +40,7 @@ export default class Peoplecard extends Component {
                             Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
                             Est eu quis anim excepteur et. ipsum
                         </h4>
-                        <p className="peopleType">Medical</p>
+                        <p className="peopleType">Nonprofit</p>
                     </div>
                     <div className="peopleCard">
                         <h4 className="peopleText">
@@ -49,7 +49,7 @@ export default class Peoplecard extends Component {
                             Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
                             Est eu quis anim excepteur et. ipsum
                         </h4>
-                        <p className="peopleType">Medical</p>
+                        <p className="peopleType">Education</p>
                     </div>
                     <div className="peopleCard">
                         <h4 className="peopleText">
@@ -58,19 +58,121 @@ export default class Peoplecard extends Component {
                             Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
                             Est eu quis anim excepteur et. ipsum
                         </h4>
-                        <p className="peopleType">Medical</p>
-                    </div>
-                    <div className="peopleCard">
-                        <h4 className="peopleText">
-                            Cillum laboris dolore sint eiusmod. 
-                            Elit ipsum commodo ad labore enim duis laborum et. 
-                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
-                            Est eu quis anim excepteur et. ipsum
-                        </h4>
-                        <p className="peopleType">Medical</p>
+                        <p className="peopleType">Animals</p>
                     </div>
                 </div>
-                
+                <div className="peopleOne">
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Environment</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Business</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Community</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Competitive</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Creative</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Event</p>
+                    </div>
+                </div>
+                <div className="peopleOne">
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Faith</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Family</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Sports</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Travel</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Volunteer</p>
+                    </div>
+                    <div className="peopleCard">
+                        <h4 className="peopleText">
+                            Cillum laboris dolore sint eiusmod. 
+                            Elit ipsum commodo ad labore enim duis laborum et. 
+                            Amet fugiat non id do ullamco qui ut veniam mollit magna non. 
+                            Est eu quis anim excepteur et. ipsum
+                        </h4>
+                        <p className="peopleType">Wishes</p>
+                    </div>
+                </div>
             </div>
         )
     }
